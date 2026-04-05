e7fabacb610598c2da006ee24922f226578aa714
Alga_Kiborg <sakinkirill@gmail.com>
2026-04-05 23:10:06 +0400

    Модель Question, миграции 0001_initial и 0002_add_initial_questions

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=500, verbose_name="Текст вопроса")
+    option1 = models.CharField(max_length=200, verbose_name="Вариант 1")
+    option2 = models.CharField(max_length=200, verbose_name="Вариант 2")
+    option3 = models.CharField(max_length=200, verbose_name="Вариант 3")
+    option4 = models.CharField(max_length=200, verbose_name="Вариант 4")
+    correct_option = models.PositiveSmallIntegerField(verbose_name="Правильный вариант (1-4)")
+    explanation = models.TextField(verbose_name="Пояснение")
+
+    def __str__(self):
+        return self.question_text
+
+    class Meta:
+        verbose_name = "Вопрос"
+        verbose_name_plural = "Вопросы"
